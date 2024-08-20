fd63817b7cdbeb29e5ba5c88668a9123fe324543
saicharan293 <2493saicharan@gmail.com>
2024-08-20 19:09:18 +0530

    useref for shifting focus back to input after clicking on add button

--- tut-1/src/AddItem.js
+++ tut-1/src/AddItem.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { FaPlus } from 'react-icons/fa'
+import { useRef } from 'react';
 
 export const AddItem = ({newItem,setnewItem,handleSubmit}) => {
+  //to re--shift focus back to input after clicking on button
+  const inputRef = useRef();
   return (
     <form className='addForm' onSubmit={handleSubmit}>
         <label htmlFor="addItem">Add Item</label>
         <input 
             type="text"
+            ref={inputRef}
             autoFocus
             id='addItem'
             placeholder='Add Item'
@@ -17,6 +21,7 @@ export const AddItem = ({newItem,setnewItem,handleSubmit}) => {
         <button 
             type='submit'
             aria-label='Add Item'
+            onClick={()=>inputRef.current.focus()}
         >
             <FaPlus />
         </button>
