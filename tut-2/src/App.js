fcac93d8a2c2072df8937fd2e7523efe23699745
saicharan293 <2493saicharan@gmail.com>
2024-08-28 19:17:01 +0530

    Completed upto about page

--- tut-2/src/App.js
+++ tut-2/src/App.js
@@ -7,14 +7,19 @@ import Missing from './Missing';
 import Footer from './Footer';
 import About from './About';
 
-import { Route,Routes,useHistory } from 'react-router-dom';
+import { Route,Routes, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Home from './Home';
+import { format } from 'date-fns/format';
 
 function App() {
   const [search, setsearch] = useState('')
   const [searchResult, setsearchResult] = useState([])
-  const history=useHistory();
+  const navigate=useNavigate();
+  const [postTitle, setpostTitle] = useState('')
+  const [postBody, setpostBody] = useState('')
+
+  
   const [posts, setposts] = useState([
     {
       id: 1,
@@ -45,16 +50,41 @@ function App() {
   const handleDelete=(id)=>{
     const postsList=posts.filter(post=>post.id!==id)
     setposts(postsList);
-    history.push('/');
+    navigate('/');
 
   }
+  useEffect(()=>{
+    const filteredPosts=posts.filter(post=>
+      ((post.body).toLowerCase()).includes(search.toLowerCase())
+      || ((post.title).toLowerCase()).includes(search.toLowerCase()))
+      setsearchResult(filteredPosts.reverse())
+      
+  },[posts,search])
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const id=posts.length?posts[posts.length - 1].id + 1 : 1;
+    const datetime=format(new Date(),'MMMM dd, yyyy pp');
+    const newPost={id,title:postTitle,datetime,body:postBody}
+    const allposts=[...posts,newPost];
+    setposts(allposts)
+    setpostTitle('');
+    setpostBody('');
+    navigate('/')
+  }
+
   return (
     <div className="App">
       <Header title="React js Blog"/>
       <Nav search={search} setsearch={setsearch}/>
       <Routes>
-        <Route path='/' element={<Home posts={posts} />} />
-        <Route path='/post' element={<Newpost />} />
+        <Route path='/' element={<Home posts={searchResult} />} />
+        <Route path='/post' element={<Newpost 
+          handleSubmit={handleSubmit}
+          postTitle={postTitle}
+          setpostTitle={setpostTitle}
+          postBody={postBody}
+          setpostBody={setpostBody}
+        />} />
         <Route path='/post/:id' element={<Postpage posts={posts} handleDelete={handleDelete}/>} />
         <Route path='/about' element={<About />} />
         <Route path='*' element={<Missing />} />
--- tut-2/src/Newpost.js
+++ tut-2/src/Newpost.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
-const Newpost = () => {
+const Newpost = ({handleSubmit,postTitle,setpostTitle,postBody, setpostBody}) => {
   console.log('new post')
   return (
-    <main>
-      <h1>New post</h1>
+    <main className='NewPost'>
+      <h2>New post</h2>
+      <form className='newPostForm' onSubmit={handleSubmit}>
+        <label htmlFor="postTitle">Title: </label>
+        <input 
+          type="text" 
+          id="postTitle" 
+          required 
+          value={postTitle} 
+          onChange={e=>setpostTitle(e.target.value)}
+        />
+        <label htmlFor="postBody">Post: </label>
+        <textarea
+         id="postBody"
+         required
+         value={postBody}
+         onChange={e=>setpostBody(e.target.value)}
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
     </main>
   )
 }
