c059bac1945c29b751a834b0706233bde2fa7536
saicharan293 <2493saicharan@gmail.com>
2024-11-27 12:31:54 +0530

    changes done for client

--- Chat-App/client/src/components/Input/Input.js
+++ Chat-App/client/src/components/Input/Input.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import closeIcon from '../../icons/closeIcon.png';
-import onlineIcon from '../../icons/onlineIcon.png';
 import './Input.css'
 
 const Input = ({message,setmessage,sendMessage}) => {
